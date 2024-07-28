6b6357d8a8559be098b76127a4508d352547e435
keerthanacm <cmkeerthana2001@gmail.com>
2024-07-28 17:35:55 +0530

    schemas for user ,admin and product

---
+++ models/userSchema.js
@@ -0,0 +1,42 @@
+const mongoose =require("mongoose")
+const userSchema = new mongoose.Schema({
+  fullName:{
+    type:String,
+    required: true,
+    minlength: [3, "Full name must be at least 3 characters long"],
+    maxlength: [64, "Full name cannot be longer than 64 characters"],
+  trim:true
+  },
+  phone:{
+    type:String,
+    required: true,
+    trim:true,
+    //  validate: {
+    //   validator: function(v) {
+    //     return /^[0-9]{8,12}$/.test(v) && !/^0+$/.test(v);
+    //   },
+    //   //message: "Phone number must be between 8 to 12 digits and cannot be all zeros"
+    // }
+  },
+  email:{
+    type:String,
+    required:true,
+    unique:true,
+    trim:true,
+   
+  },
+  password:{
+    type:String,
+    required:true
+  },
+  isDeleted: {
+     type: Boolean,
+     default: false 
+  },
+  status: {
+    type: String,
+    default: "active",
+  }
+})
+
+module.exports = mongoose.model("User", userSchema);
